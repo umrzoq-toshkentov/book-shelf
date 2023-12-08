9a12c1eb037842c8e263caee14176d26b5d1179e
Umrzoq <umrzoq@superdispatch.com>
2023-12-08 15:46:24 +0500

    feat: Formik and Hook Form examples

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,20 @@
-import { Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
+import { HookForm } from "../../shared/modules/HookForm";
+import { FormikForm } from "../../shared/modules/FormikForm";
 
 export function HomePage() {
   return (
     <Stack>
-      <Typography variant="h1">Home Page</Typography>
-      <Typography variant="body1">
-        This is the home page. You can edit this page in{" "}
-        <code>src/pages/Home/HomePage.tsx</code>.
-      </Typography>
+      <Typography variant="h3">Form validation Page</Typography>
+      <Grid spacing={4} container>
+        <Grid item xs={12} md={6}>
+          <HookForm />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <FormikForm />
+        </Grid>
+      </Grid>
     </Stack>
   );
 }
---
+++ src/shared/modules/FormikForm/FormikForm.tsx
@@ -0,0 +1,58 @@
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Field, useFormik } from "formik";
+import * as zod from "zod";
+
+const validationSchema = zod.object({
+  email: zod
+    .string({ description: "Enter your email" })
+    .email("Enter a valid email"),
+  password: zod
+    .string({ description: "Enter your password" })
+    .min(8, "Password should be of minimum 8 characters length"),
+});
+
+export const FormikForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "foobar@example.com",
+      password: "foobar",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <Stack component="form" onSubmit={formik.handleSubmit} spacing={2}>
+      <Typography variant="h5">Formik Form</Typography>
+
+      <TextField
+        fullWidth
+        id="email"
+        name="email"
+        label="Email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+        helperText={formik.touched.email && formik.errors.email}
+      />
+      <TextField
+        fullWidth
+        id="password"
+        name="password"
+        label="Password"
+        type="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helperText={formik.touched.password && formik.errors.password}
+      />
+      <Button color="primary" variant="contained" fullWidth type="submit">
+        Submit
+      </Button>
+    </Stack>
+  );
+};
